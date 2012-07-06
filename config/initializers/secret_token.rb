4654be2838a3b9e6e4d371a9adea31e24f02cec8
Thomas Boyce <tboyce@stanford.edu>
2012-07-06 11:28:59 -0700

    Basic login system with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chooseit::Application.config.secret_token = '1f9a393fc0b5019445eb94c4f937edb472bc4c41684f73ef72b0c211efb7e568a61109dcee3c224cb223927a94f1e991416f4d7c63abfe0f778dc959c0be73de'
