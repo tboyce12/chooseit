501321c3ab898e7052355ed9789f1d6b149ca82f
Thomas Boyce <tboyce@stanford.edu>
2012-07-12 13:34:15 -0700

    close #11

--- app/models/tot.rb
+++ app/models/tot.rb
@@ -7,7 +7,7 @@ class Tot < ActiveRecord::Base
   validates_length_of :a, :maximum => 30
   validates_length_of :b, :maximum => 30
   
-  scope :not_created_by, lambda { |user| {:conditions => ["user_id <> ?", user.id]} }
+  # scope :not_created_by, lambda { |user| {:conditions => ["user_id <> ?", user.id]} }
   
   # Methods for counting votes
   def a_votes
@@ -20,16 +20,31 @@ class Tot < ActiveRecord::Base
   # Find the specified record, ensuring that it doesn't belong to user
   # and user hasn't voted on it
   def self.find_by_id_not_touched_by(id, user)
-    tot = Tot.find_by_id(id)
-    return tot unless tot.user_id == user.id
+    not_touched_by(user).each do |tot|
+      return tot if tot.id == id.to_i
+    end
   end
   
   # Find a random record that doesn't belong to user and hasn't been voted on by user
   def self.find_random_not_touched_by(user)
-    tots = Tot.not_created_by(user)
-    tot = tots[rand(tots.length)] unless tots.blank?
+    tots = not_touched_by(user)
+    tots[rand(tots.length)] unless tots.blank?
     
     # ids = connection.whereselect_all("SELECT id FROM tots WHERE )
     # find(ids[rand(ids.length)]["id"].to_i) unless ids.blank?
   end
+  
+  # Find records that user doesn't own and hasn't voted on
+  def self.not_touched_by(user)
+    # Find records not owned by user
+    tots = Tot.joins('INNER JOIN users ON users.id <> tots.user_id').where('users.id = ?', user.id).order('tots.id ASC')
+    # For each record, add to array if user has NOT voted on it
+    result = Array.new
+    tots.each do |tot|
+      vote = Vote.where('user_id = ? AND tot_id = ?', user.id, tot.id).first
+      result.push tot unless vote
+    end
+    result
+  end
+  
 end
