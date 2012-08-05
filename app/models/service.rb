676fc9847f0ea98c975d0579bc6a412c1dd686e7
Thomas Boyce <tboyce@stanford.edu>
2012-08-04 21:36:18 -0700

    Add validations and some unit tests for services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,7 @@
 class Service < ActiveRecord::Base
   belongs_to :user
   attr_accessible :provider, :uemail, :uid, :uname, :user_id
+  
+  validates_uniqueness_of :uid, :scope => :provider
+  validates_presence_of :provider, :uid, :user_id
 end
--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -4,4 +4,20 @@ class ServiceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "service must belong to a user" do
+    orphan = Service.create :provider => 'facebook', :uid => '12345'
+    tom = users :tom
+    child = tom.services.create :provider => 'facebook', :uid => '23456'
+    assert !orphan.persisted?, "Saved a service without an owning user"
+    assert child.persisted?, "Failed to save a service belonging to a user"
+  end
+  
+  test "cant have two services with same provider and uid" do
+    tom = users :tom
+    a = tom.services.create :provider => 'facebook', :uid => '12345'
+    b = tom.services.create :provider => 'facebook', :uid => '12345'
+    assert a.persisted?, "Failed to save first service"
+    assert !b.persisted?, "Saved a duplicate service"
+  end
 end
